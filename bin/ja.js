53a1288a468ef4b9133275494df3a902663aaa66
saasan <saasan.s2works@hotmail.com>
2015-09-29 14:27:04 +0900

    リンク

--- bin/ja.js
+++ bin/ja.js
@@ -4,25 +4,29 @@ exports.scheduleMessages = [
 A、Eグループ：①8時、②12時、③19時
 B、Fグループ：①9時、②13時、③20時
 C、Gグループ：①10時、②14時、③21時
-D、Hグループ：①11時、②15時、③22時`,
+D、Hグループ：①11時、②15時、③22時
+http://saasan.github.io/imas/trainer-ticket-time/`,
 // パターンB
 `本日のトレチケタイムは以下の通りです。
 A、Eグループ：①9時、②13時、③20時
 B、Fグループ：①10時、②14時、③21時
 C、Gグループ：①11時、②15時、③22時
-D、Hグループ：①8時、②12時、③19時`,
+D、Hグループ：①8時、②12時、③19時
+http://saasan.github.io/imas/trainer-ticket-time/`,
 // パターンC
 `本日のトレチケタイムは以下の通りです。
 A、Eグループ：①10時、②14時、③21時
 B、Fグループ：①11時、②15時、③22時
 C、Gグループ：①8時、②12時、③19時
-D、Hグループ：①9時、②13時、③20時`,
+D、Hグループ：①9時、②13時、③20時
+http://saasan.github.io/imas/trainer-ticket-time/`,
 // パターンD
 `本日のトレチケタイムは以下の通りです。
 A、Eグループ：①11時、②15時、③22時
 B、Fグループ：①8時、②12時、③19時
 C、Gグループ：①9時、②13時、③20時
-D、Hグループ：①10時、②14時、③21時`
+D、Hグループ：①10時、②14時、③21時
+http://saasan.github.io/imas/trainer-ticket-time/`
 ];
 
 exports.groupTable = [
@@ -32,4 +36,6 @@ exports.groupTable = [
   ['D、H', 'C、G', 'B、F', 'A、E']
 ];
 
-exports.startSoon = '【%(hours)d時】から【%(group)s】グループのトレチケタイムが始まります。';
+exports.startSoon =
+`【%(hours)d時】から【%(group)s】グループのトレチケタイムが始まります。
+今日のトレチケタイム：http://saasan.github.io/imas/trainer-ticket-time/`;
